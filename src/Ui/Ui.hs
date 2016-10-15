c07e1cee09fb5c151a38b2e7f3e7bd7fa305360d
Bernhard Biskup <bbiskup@gmx.de>
2016-10-15 18:46:24 +0200

    basic main widget

--- src/Ui/Ui.hs
+++ src/Ui/Ui.hs
@@ -1,20 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Ui where
+
 import Data.Monoid((<>))
 import Data.Text.Markup((@@))
 
 import qualified Graphics.Vty as V
 import qualified Brick.Types as BT
-import qualified Brick.Widgets.Core as BWC
 import Brick.Markup(markup, (@?))
 import Brick.Util (on, fg)
 import Brick.Widgets.Core(
       (<+>)
     , (<=>)
+    , str
     )
+import Brick.AttrMap (attrMap, AttrMap)
+
 
 ui :: BT.Widget ()
-ui = (m1 <=> m2) <+> (BWC.padLeft (BT.Pad 1) m3)
-    where
-        m1 = markup $ ("Hello1" @@ fg V.blue) <> ", " <> ("world1!" @@ fg V.red)
+ui = str "Hello"
 
 
 theMap :: AttrMap
