2889b0cf5902827a4ab9ebb0ea06ed989e371808
Bernhard Biskup <bbiskup@gmx.de>
2016-10-15 18:37:42 +0200

    added attrMap

--- src/Ui/Ui.hs
+++ src/Ui/Ui.hs
@@ -15,5 +15,10 @@ ui :: BT.Widget ()
 ui = (m1 <=> m2) <+> (BWC.padLeft (BT.Pad 1) m3)
     where
         m1 = markup $ ("Hello1" @@ fg V.blue) <> ", " <> ("world1!" @@ fg V.red)
-        m2 = markup $ ("Hello2" @? "keyword1") <> ", " <> ("world2!" @? "keyword2")
-        m3 = markup $ ("Hello3," @? "keyword1") <> "\n" <> ("world3!" @? "keyword2")
+
+
+theMap :: AttrMap
+theMap = attrMap V.defAttr
+    [ ("keyword1",      fg V.yellow)
+    , ("keyword2",      V.white `on` V.blue)
+    ]
