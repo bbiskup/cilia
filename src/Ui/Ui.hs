d044f2af63a990d06b6740714a9635fc0a77fc3d
Bernhard Biskup <bbiskup@gmx.de>
2016-10-15 18:47:23 +0200

    added Brick application object

--- src/Ui/Ui.hs
+++ src/Ui/Ui.hs
@@ -15,6 +15,7 @@ import Brick.Widgets.Core(
     , str
     )
 import Brick.AttrMap (attrMap, AttrMap)
+import Brick.Main (App(..), defaultMain, resizeOrQuit, neverShowCursor)
 
 
 ui :: BT.Widget ()
@@ -26,3 +27,13 @@ theMap = attrMap V.defAttr
     [ ("keyword1",      fg V.yellow)
     , ("keyword2",      V.white `on` V.blue)
     ]
+
+app :: App () V.Event ()
+app =
+    App { appDraw = const [ui]
+        , appHandleEvent = resizeOrQuit
+        , appAttrMap = const theMap
+        , appStartEvent = return
+        , appChooseCursor = neverShowCursor
+        , appLiftVtyEvent = id
+        }
