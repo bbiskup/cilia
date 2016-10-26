0a3846c0ffc7df401adeebf684623b73b69a0d8d
Bernhard Biskup <bbiskup@gmx.de>
2016-10-26 07:44:23 +0200

    rearranged directory structure and project components (library component)

--- src/Main.hs
+++ src/Main.hs
@@ -1,64 +1,7 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Prelude
-import Data.Monoid((<>))
-import Control.Monad(void)
-import Control.Concurrent(newChan, forkIO)
-import Data.Time.Clock(getCurrentTime)
-import Data.Default(def)
-import Brick.Main(customMain)
-import qualified Graphics.Vty as V
-import Options.Applicative
-
-import Opts( Opts(..)
-           , optsParser)
-import Ui( AppState(..)
-         , app)
-import qualified Ci
-import Config(Config, readConfig)
-
-
-{-dummyRepos :: [Repo]
-dummyRepos = [ 
-        Repo {
-               _slug = Just "myslug"
-             , _description = Just "mydescription"
-             , _lastBuildState = Just Passed
-             , _lastBuildNumber = Just "mylastbuildnumber"
-             , _lastBuildDuration = Just 20
-             , _lastBuildFinishedAt = Just "myfinishedat"
-             , _active = Just True
-             }
-        ]
--}
-
-initialState :: Config -> IO Ui.AppState
-initialState config = do 
-    timestamp <- getCurrentTime
-    return Ui.AppState 
-        { _conf = config
-        , _stLastVtyEvent = Nothing
-        , _repos = []
-        , _timestamp = timestamp
-        , _errMsg  = Nothing
-        }
-
-doMain :: Opts -> IO ()
-doMain (Opts configFileName') = do
-    config <- Config.readConfig configFileName'
-    chan <- newChan
-    initialState' <- initialState config
-    _ <- forkIO $ Ci.checkCIServers config chan
-    void $ customMain (V.mkVty def) chan app initialState'
+import LibMain(libMain)
 
 main :: IO ()
-main = do
-    optsParser' <- optsParser
-    let opts = info (helper <*> optsParser')
-            (fullDesc
-            <> progDesc "Run cilia"
-            <> header "cilia - continuous integration monitor")
-    execParser opts >>= doMain
-        
-        
+main = libMain
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,13 @@
 import Test.Hspec
 
+import Types(BuildState(Passed))
+
 main :: IO()
 main = hspec $ do
     describe "test 1" $ do
         it "addition" $ do
             1 + 1 `shouldBe` 2
+
+    describe "BuildState Show instance" $ do
+        it "show Passed" $ do
+            (show Passed) `shouldBe` "Passed"
